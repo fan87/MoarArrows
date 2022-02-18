a52ed21fa776b4f2f3c0950ceac7b485b788f8d9
fan87 <fan87.TW@gmail.com>
2022-02-18 09:04:39 +0800

    Initalized Project

---
+++ src/main/java/com/dragoncommissions/moararrows/MoarArrows.java
@@ -0,0 +1,17 @@
+package com.dragoncommissions.moararrows;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoarArrows extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
